8bccfcff69c8f9b14d04f2babfcd5e8eae95f162
Durbar Roy <durbarroy681@gmail.com>
2024-03-28 02:55:05 +0530

    Add files via upload

---
+++ emacs-custom1.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(tango-dark))
+ '(org-fold-core-style 'overlays)
+ '(package-selected-packages
+   '(auctex nov djvu org-pdftools pdf-tools eradio markdown-mode hyperbole yasnippet org-roam-bibtex ivy-bibtex org-ref org-roam-ui org-roam consult which-key swiper company marginalia orderless olivetti vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight ((t (:background "gray10" :foreground "lime green")))))
+;; (custom-set-faces '(vertico-current ((t (:background “blue”)))))
+
