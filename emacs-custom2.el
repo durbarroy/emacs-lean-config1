fe0e8624a0187ae601488832983b088b534683c9
Durbar Roy <durbarroy681@gmail.com>
2024-04-06 15:20:32 +0530

    Add files via upload

---
+++ emacs-custom2.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil)
+ '(org-fold-core-style 'overlays)
+ '(package-selected-packages
+   '(vterm hide-mode-line hide-mode-line-mode auctex nov djvu org-pdftools pdf-tools eradio markdown-mode hyperbole yasnippet org-roam-bibtex ivy-bibtex org-ref org-roam-ui org-roam consult which-key swiper company marginalia orderless olivetti vertico)))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(highlight ((t (:background "gray10" :foreground "lime green")))))
+;; (custom-set-faces '(vertico-current ((t (:background “blue”)))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight ((t (:background "gray10" :foreground "lime green")))))
