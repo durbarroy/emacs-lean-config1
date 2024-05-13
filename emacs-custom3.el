dc151f3bc9e404f0080cd2438b5b714ae83d8119
Durbar Roy <durbarroy681@gmail.com>
2024-05-13 12:27:37 +0530

    Add files via upload

---
+++ emacs-custom3.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(standard-light))
+ '(custom-safe-themes
+   '("0c5d7ffa7cdf8b889342d2cd38f6ddd2cc716561048feca89b17fda56677d6b8" "8122fb61548fe36171d9cf24cdb9b5f24d053b626d4cda739c3815e080623209" "9f27d5afd6d78b40bf1290c10722818e0b90f141fc3758d3c2d284ccb565de15" "7776ba149258df15039b1f0aba4b180d95069b2589bc7d6570a833f05fdf7b6d" "d4b608d76e3a087b124c74c2b642c933d8121b24e53d4bbd5e7327c36cc69ccc" "aa688776604bbddbaba9e0c0d77e8eb5f88d94308f223d1962b6e6b902add6a0" "263e3a9286c7ab0c4f57f5d537033c8a5943e69d142e747723181ab9b12a5855" "841b6a0350ae5029d6410d27cc036b9f35d3bf657de1c08af0b7cbe3974d19ac" default))
+ '(org-fold-core-style 'overlays)
+ '(package-selected-packages
+   '(org-inline-anim standard-themes ef-themes vterm hide-mode-line hide-mode-line-mode auctex nov djvu org-pdftools pdf-tools eradio markdown-mode hyperbole yasnippet org-roam-bibtex ivy-bibtex org-ref org-roam-ui org-roam consult which-key swiper company marginalia orderless olivetti vertico)))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(highlight ((t (:background "gray10" :foreground "lime green")))))
+;; (custom-set-faces '(vertico-current ((t (:background “blue”)))))
+
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(highlight ((t (:background "gray10" :foreground "lime green")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
